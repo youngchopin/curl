10c0a9802c081e1b2fbfd05102d99202e22e45e7
youngchopin <youngchopin@users.noreply.github.com>
2017-11-29 11:46:47 +0800

    Update rtsp.c

--- docs/examples/rtsp.c
+++ docs/examples/rtsp.c
@@ -129,6 +129,7 @@ static void rtsp_play(CURL *curl, const char *uri, const char *range)
   my_curl_easy_setopt(curl, CURLOPT_RANGE, range);
   my_curl_easy_setopt(curl, CURLOPT_RTSP_REQUEST, (long)CURL_RTSPREQ_PLAY);
   my_curl_easy_perform(curl);
+	my_curl_easy_setopt(curl, CURLOPT_RANGE, NULL);
 }
 
 
